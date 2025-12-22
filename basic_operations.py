0f0fbb7889adbe595ae0843b964cfe599b29bd18
Kidus-Nesibu <knesibu3@gmail.com>
2025-12-22 12:35:41 +0300

    python task 0

---
+++ basic_operations.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+number1, number2 = 10, 5
+print(f"Addition of {number1} and {number2} is", number1 + number2)
+print(f"Substraction of {number1} and {number2} is", number1 - number2)
+print(f"Multiplication of {number1} and {number2} is", number1 * number2)
