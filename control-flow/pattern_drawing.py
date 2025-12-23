c804e9fb4e58b6d01a8aff0c875e65a26127218d
Kidus-Nesibu <knesibu3@gmail.com>
2025-12-23 11:42:36 +0300

    control flow task 3

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 size = abs(int(input("Enter the size of the pattern: ")))
 
-for i in range(size):
+index = size
+while index:
     for i in range(size):
         print("*", end="")
     print("")
+    index -= 1
