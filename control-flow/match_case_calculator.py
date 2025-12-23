d8ae0da8d70c9dbc8599ac2fa474b2dbb178c42b
Kidus-Nesibu <knesibu3@gmail.com>
2025-12-23 11:22:44 +0300

    control flow task 1

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter  the second number: "))
+num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /):")
 result = 0
 
