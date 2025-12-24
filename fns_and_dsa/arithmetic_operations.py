275ea2848562e685f511453a735613447f49fb9a
Kidus-Nesibu <knesibu3@gmail.com>
2025-12-24 15:36:14 +0300

    Function, Data Structures and Modules task 0

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perfom_operations(num1, num2, operation):
+
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 != 0:
+                return num1 / num2
+            elif num2 == 0:
+                return "The second number can not be zero"
