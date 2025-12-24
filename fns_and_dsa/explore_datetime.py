eb7743d86296fa6269fc9eab2050244a092ff91f
Kidus-Nesibu <knesibu3@gmail.com>
2025-12-24 17:00:32 +0300

    Functions, Data Structures and Modules task 2

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current Date and Time:", formatted_date)
+    return current_date  
+
+def calculate_future_date():
+    try:
+        days_to_add = int(input("Enter the number of days to add: "))
+    except ValueError:
+        print("Please enter a valid integer.")
+        return
+
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days_to_add)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Date after {days_to_add} days will be:", formatted_future_date)
+    return future_date
