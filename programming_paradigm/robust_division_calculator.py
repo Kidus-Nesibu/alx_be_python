a6ccd2d86803f460db77ba84a9d23c929f4a3a17
Kidus-Nesibu <knesibu3@gmail.com>
2025-12-31 20:13:45 +0300

    Error Handling task 1

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 def safe_divide(numerator, denominator):
     try:
-        return "The result of the division is " + float(numerator) / float(denominator)
+        return "The result of the division is " + str(float(numerator) / float(denominator))
     except ValueError:
         return "Error: Please enter numeric values only."
     except ZeroDivisionError:
